7a9b3c180ce9bc294b762bc043e4020f7359c9ea
Rohan Thakur <131163309+rohanthakur336@users.noreply.github.com>
2024-08-24 23:52:15 +0530

    Delete Restaurant_Bot/src directory

--- Restaurant_Bot/src/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createOrder } = require('../controllers/orderController');
-
-const router = express.Router();
-
-router.post('/orders', createOrder);
-
-module.exports = router;
--- Restaurant_Bot/src/routes/restaurantRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getRestaurants, getRestaurantMenu } = require('../controllers/restaurantController');
-
-const router = express.Router();
-
-router.get('/restaurants', getRestaurants);
-router.get('/restaurants/:id/menu', getRestaurantMenu);
-
-module.exports = router;
