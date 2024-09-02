1bc5f04e6104b474dacb1e1c9b374087968066d6
Rohan Thakur <rthakur_be21@thapar.edu>
2024-09-02 19:32:26 +0530

    updates on server.js

--- food-app/server.js
+++ food-app/server.js
@@ -4,26 +4,40 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
-//dot env config
+
+//dot en configuration
 dotenv.config();
-//db connection
+
+//DB connection
 connectDb();
+
 //rest object
 const app = express();
-//middleware
+
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
 //route
+// URL => http://localhost:8080
 app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
-//URL => http://localhost:8080/
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/resturant", require("./routes/resturantRoutes"));
+app.use("/api/v1/category", require("./routes/catgeoryRoutes"));
+app.use("/api/v1/food", require("./routes/foodRoutes"));
+
 app.get("/", (req, res) => {
-  return res.status(200).send("<h1>Welcome to Food Server</h1>");
+  return res
+    .status(200)
+    .send("<h1>Welcome to Food Server APP API BASE PROJECT </h1>");
 });
+
 //PORT
 const PORT = process.env.PORT || 8080;
-//Listen
+
+//listen
 app.listen(PORT, () => {
-  console.log(`Server is started at http://localhost:${PORT}`.bgCyan.red);
+  console.log(`Server running on ${PORT}`.white.bgMagenta);
 });
